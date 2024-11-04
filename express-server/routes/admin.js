d0830200c3a6eb198d4fb51240939a72ba93688a
krishnarrana <krishna2121.rana@gmail.com>
2024-11-04 16:26:28 +0100

    Day 4 - Template Engine

--- express-server/routes/admin.js
+++ express-server/routes/admin.js
@@ -4,14 +4,19 @@ const router = express.Router();
 
 const rootDir = require("../util/path");
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(
-    path.join(rootDir, "express-server", "../", "views", "add-product.html")
-  );
+  res.render("add-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- express-server/routes/shop.js
+++ express-server/routes/shop.js
@@ -2,12 +2,15 @@ const path = require("path");
 const rootDir = require("../util/path");
 const express = require("express");
 
+const adminData = require("./admin.js");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(
-    path.join(rootDir, "express-server", "../", "views", "shop.html")
-  );
+  console.log("Admin data", adminData.products);
+
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
